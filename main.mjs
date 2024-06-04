34039f54127c31e34667ff2ace159c09f67ac12e
DanielKlimovOff <danielklimovoff@gmail.com>
2024-06-05 00:44:02 +0700

    hello world XD

---
+++ main.mjs
@@ -0,0 +1,10 @@
+import { } from 'game/utils';
+import { } from 'game/prototypes';
+import { } from 'game/constants';
+import { } from 'arena';
+
+export function loop() {
+    // Your code goes here
+    console.log('hello world');
+    return;
+}
